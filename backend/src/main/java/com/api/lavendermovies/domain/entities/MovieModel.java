c0ca40d3f66f3dbc7dd0aa41d194ac2af480bde6
Gabriela Hermenegildo Junior <gabriela.hermenegil@meta.com.br>
2022-10-25 14:06:27 -0300

    starting movie model

--- backend/src/main/java/com/api/lavendermovies/domain/entities/MovieModel.java
+++ backend/src/main/java/com/api/lavendermovies/domain/entities/MovieModel.java
@@ -2,6 +2,7 @@ package com.api.lavendermovies.domain.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -15,4 +16,19 @@ public class MovieModel implements Serializable {
 
     @Column(nullable = false, length = 30)
     private String title;
+
+    @Column(nullable = false)
+    private String summary;
+
+    @Column
+    private double rating;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private int duration;
 }
