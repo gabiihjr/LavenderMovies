c5bdd126600f65dc30ba1f4c314c78b9b9c37dac
Gabriela Hermenegildo Junior <gabriela.hermenegil@meta.com.br>
2022-11-03 16:47:28 -0300

    role and user

--- backend/src/main/java/com/api/lavendermovies/repository/RoleRepository.java
+++ backend/src/main/java/com/api/lavendermovies/repository/RoleRepository.java
@@ -1,11 +1,14 @@
 package com.api.lavendermovies.repository;
 
 import com.api.lavendermovies.domain.models.Role;
+import com.api.lavendermovies.enums.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, UUID> {
+    Optional<Role> findByRoleName(RoleName roleName);
 }
--- backend/src/main/java/com/api/lavendermovies/service/UserService.java
+++ backend/src/main/java/com/api/lavendermovies/service/UserService.java
@@ -1,24 +1,41 @@
 package com.api.lavendermovies.service;
 
+import com.api.lavendermovies.config.exceptions.BusinessException;
 import com.api.lavendermovies.config.security.WebSecurityConfig;
+import com.api.lavendermovies.domain.models.Role;
 import com.api.lavendermovies.domain.models.User;
 import com.api.lavendermovies.dtos.GetUserDto;
+import com.api.lavendermovies.enums.RoleName;
 import com.api.lavendermovies.forms.UserForm;
+import com.api.lavendermovies.repository.RoleRepository;
 import com.api.lavendermovies.repository.UserRepository;
 import com.api.lavendermovies.utils.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    RoleRepository roleRepository;
+
     public GetUserDto save(UserForm userForm) {
         var password = WebSecurityConfig.passwordEncoder().encode(userForm.getPassword());
         userForm.setPassword(password);
 
         var user = ObjectMapper.map(userForm, User.class);
+        var role = roleRepository.findByRoleName(RoleName.ADMIN)
+                .orElseThrow(() -> new BusinessException("Role not found."));
+
+        var roleList = new ArrayList<Role>();
+        roleList.add(role);
+
+        user.setRoles(roleList);
+
         userRepository.save(user);
 
         return ObjectMapper.map(userForm, GetUserDto.class);
