f169cfae8e820cdc113ad132311298eadf0c2885
Gabriela Hermenegildo Junior <gabriela.hermenegil@meta.com.br>
2022-11-22 17:30:22 -0300

    frontend beginning

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // template: '<h1>Hello World from Inline Template!</h1>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Lavender Movies';
+}
